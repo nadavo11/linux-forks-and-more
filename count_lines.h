2f96a324a3d19c5a016870f61c03baec740d98a9
lior <lior922@gmail.com>
2022-11-19 13:15:56 +0200

    add one .h file

---
+++ count_lines.h
@@ -0,0 +1,11 @@
+//
+// Created by lior on 11/19/22.
+//
+void count_lines (short unsigned int G)
+
+
+
+#ifndef UNTITLED5_COUNT_LINES_H
+#define UNTITLED5_COUNT_LINES_H
+
+#endif //UNTITLED5_COUNT_LINES_H
